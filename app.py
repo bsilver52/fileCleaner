49ec69e3b4ad3ef297aa986b9abed3c6f357ce7a
Benjamin Silver <108251033+bsilver52@users.noreply.github.com>
2024-08-11 15:36:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+server = app.server
 
 app.layout = html.Div([
     html.H2("Ben's File Cleaning App", style={'textAlign': 'center'}),
